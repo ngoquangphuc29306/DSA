9be2295f0797a39eddaf228f91582c9a49911114
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-06-04 23:34:20 +0700

    Time: 0 ms (100.00%) | Memory: 10 MB (46.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size()) return false;
-        
+        if (s1.size() > s2.size())
+            return false;
+
         unordered_map<char, int> cnt;
 
-        for(char c : s1) cnt[c]++;
+        for (char c : s1)
+            cnt[c]++;
 
         int l = 0;
         int n_letters = s1.size();
 
-        for(int r = 0; r < s2.size(); r++){
+        for (int r = 0; r < s2.size(); r++) {
             int n = cnt[s2[r]];
             cnt[s2[r]]--;
 
-            if(n > 0){
-                // Không nằm trong s1
+            if (n > 0) {
+                // Ký tự này đang cần thiết để tạo hoán vị của s1
                 n_letters--;
-            }
-            else{
-                while(cnt[s2[r]] < 0){
+            } else {
+                while (cnt[s2[r]] < 0) {
                     cnt[s2[l]]++;
-                    
-                    if(cnt[s2[l]] > 0){
+
+                    if (cnt[s2[l]] > 0) {
                         // Tránh trường hợp subfrequence
                         n_letters++;
                     }
@@ -30,7 +31,8 @@ public:
                 }
             }
 
-            if(n_letters == 0) return true;
+            if (n_letters == 0)
+                return true;
         }
         return false;
     }
