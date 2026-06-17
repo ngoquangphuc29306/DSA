b00d166d7a8b5ce5aab15da2610dd6cf014f129d
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-06-17 22:32:14 +0700

    Time: 3 ms (43.00%) | Memory: 12.7 MB (20.03%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,33 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        slots = n * 2
-        # Recursive
-        def generate(sofar = [""]):
-            if len(sofar[0]) == slots:
-                return sofar
-
-            L = []
-            for paren in sofar:
-                L.append(paren + "(")
-                L.append(paren + ")")
-            return generate(L)
-
-        L = generate()
-
-        def is_valid(paren):
-            op = 0
-            for c in paren:
-                if c == "(":
-                    op += 1
-                else:
-                    if op == 0:
-                        return False
-                    else:
-                        op -= 1
-            return op == 0
-        
         ans = []
-        return [paren for paren in L if is_valid(paren)]
-
-
+        # Recursive
+        def generate(sofar = "", op = 0, cl = 0):
+            if op > n: return
+            if cl > op: return
+            if cl == op == n:
+                ans.append(sofar)
+            generate(sofar + "(", op + 1, cl)
+            generate(sofar + ")", op, cl + 1)
+        generate()
+        return ans
