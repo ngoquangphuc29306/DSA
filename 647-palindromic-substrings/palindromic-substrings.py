028e9dfc1e196859dde31b4df9b6d2ddaea10db7
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-06-22 20:12:02 +0700

    Time: 250 ms (29.36%) | Memory: 12.3 MB (65.68%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: int
         """
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
+        dp = [False] * n
         cnt = 0
 
         for j in range(len(s)):
             for i in range(j + 1):
-                dp[i][j] = (s[i] == s[j]) and (j - i <= 2 or dp[i + 1][j - 1])
+                dp[i] = s[i] == s[j] and (j - i <= 2 or dp[i + 1])
 
-                if dp[i][j]:
+                if dp[i]:
                     cnt += 1
         return cnt
 
