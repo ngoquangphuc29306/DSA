b3da76ad678dc38c5a1af572e492a2422e2156c0
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-09-16 13:50:52 +0700

    Time: 1 ms (82.25%) | Memory: 12.2 MB (40.80%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
 
     bool backtrack(int index, int cur_sum, int target, int k_left, vector<bool>& used, vector<int>& nums){
+        // Đã gom đủ k - 1 thùng thì thùng cuối cùng tự động thỏa mãn
         if(k_left == 1) return true;
 
+        // Nếu thùng hiện tại đã đầy, Chuyển sang lấp thùng tiếp theo từ đầu
         if(cur_sum == target){
             return backtrack(0, 0, target, k_left - 1, used, nums);
         }
 
         for(int i = index; i < nums.size(); i++){
             if(used[i] || cur_sum + nums[i] > target) continue;
+            
+            // Bỏ qua số trùng lặp nếu số giống hệt trước đó vừa thất bại
+            if (i > index && nums[i] == nums[i - 1] && !used[i - 1]) continue;
 
             used[i] = true;
             if(backtrack(i + 1, cur_sum + nums[i], target, k_left, used, nums)){
